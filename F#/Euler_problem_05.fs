806d6f366aa47d78d8ffe2700780b4f72e4b70db
Klaus78 <tagliole@libero.it>
2018-03-20 21:47:24 +0100

    Euler problem 05

---
+++ F#/Euler_problem_05.fs
@@ -0,0 +1,19 @@
+﻿// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+// functions that returns true if n can be divided by each
+// of the numbers from 1 to m
+let isEvenlyDivisible n m =
+    seq{1..m} |>
+    Seq.filter(fun i -> n%i = 0)
+    |> Seq.length = m
+	
+let maxDiv = 20
+let res = 
+	// generate sequence 0, 20, 40, 60 ....
+	Seq.initInfinite(fun n -> n*maxDiv)
+	|> Seq.filter(fun n -> n > 0)
+	|> Seq.filter(fun n -> isEvenlyDivisible n maxDiv)
+	|> Seq.head
+		
+// result is 232792560
+printfn "%A" res
